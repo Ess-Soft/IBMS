f41e768cff95af58b2012d8dff10d1dc77ba8125
ess-soft <essare.ayoub@gmail.com>
2018-03-22 19:32:56 +0000

    adding data validation in HomeFilterActivity

--- app/src/main/java/com/tdigroup/istaabsense/HomeFilterActivity.java
+++ app/src/main/java/com/tdigroup/istaabsense/HomeFilterActivity.java
@@ -13,29 +13,33 @@ import butterknife.BindView;
 
 public class HomeFilterActivity extends AppCompatActivity {
 
+    /**
     @BindView(R.id.niveau_switch)
     ToggleSwitch niveauSwitch;
 
     @BindView(R.id.level_year_switch)
-    ToggleSwitch levelYearSwitch;
+    ToggleSwitch levelYearSwitch;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_filter);
 
-        /** check for selected niveau: T.S or T */
+        /** check for selected niveau: T.S or T
         if (niveauSwitch.getCheckedTogglePosition() == 0){
             VariablesData.TYPE_TECHNECIEN_SPECIALISE = true;
         } else if (niveauSwitch.getCheckedTogglePosition() == 1){
             VariablesData.TYPE_TECHNECIEN_SPECIALISE = true;
         }
 
-        /** check for year : 1er anne or 2eme anne */
+        /** check for year : 1er anne or 2eme anne
         if (levelYearSwitch.getCheckedTogglePosition() == 0){
             VariablesData.TYPE_1erAnne =true;
         } else if (levelYearSwitch.getCheckedTogglePosition() == 1){
             VariablesData.TYPE_2emeAnne = true;
-        }
+         */
+
+
+
 
 
 
